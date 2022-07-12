7d83e745d7cd2e345d3cd96a69b6c79aae07b9e9
孟庆贺 <122274389@qq.com>
2022-07-12 11:34:10 +0800

    更新demo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import * as Vue from "vue";
 import { useModule } from "vue-module-loader";
 import localModule from "./module";
 useModule(
-  "http://static.mengqinghe.com/vml/frame/vue-module-frame.iife.js",
-  Vue
+  "http://static.mengqinghe.com/vml/frame/vue-module-frame.iife.js"
 ).then(() => {
   useModule(localModule);
 });
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <p>这里是模块工程页面</p>
-    <img src="../img/byk.jpg" />
   </div>
 </template>
 <style scoped>
